e8d6e176c27bf087bcf81e67e236d4e354c9d75a
ljx <ljx@localhost>
2017-11-26 15:00:38 +0800

    编写接口实现代码

--- src/com/seeyon/apps/test2/TestManager.java
+++ src/com/seeyon/apps/test2/TestManager.java
@@ -1,5 +1,6 @@
 package com.seeyon.apps.test2;
 
 public interface TestManager {
-
+	public String getName();
+	public String showListMember();
 }
--- src/com/seeyon/apps/test2/TestManagerImpl.java
+++ src/com/seeyon/apps/test2/TestManagerImpl.java
@@ -2,4 +2,16 @@ package com.seeyon.apps.test2;
 
 public class TestManagerImpl implements TestManager {
 
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String showListMember() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
